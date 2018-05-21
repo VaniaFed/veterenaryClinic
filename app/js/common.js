1d9ca7503fc9d55da8aad52e07030391c9c4da83
Vania Fedyakov <vaniafed@localhost.localdomain>
2018-05-21 20:30:39 +0300

    services is begined

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,4 @@
+'use strict';
 $(function() {
 	// Пользовательские функции
     
@@ -19,4 +20,24 @@ $(function() {
         });
             return false;
     });
+
+    
+    var widthEqualHeight = function (el) {
+
+        var width = el[1].offsetWidth;
+
+        for (var i = 0; i < el.length; i++) {
+            el[i].style.height = width + 'px';
+        }
+    }
+
+    if (document.documentElement.clientWidth > 1024) {
+        var el = document.querySelectorAll(".services-item");
+
+        widthEqualHeight (el);
+
+        window.resize = function() {
+            widthEqualHeight (el);
+        }
+    }
 });
