c3c0c4200c794f99076b5397ed35a627fd445356
Vania Fedyakov <vaniafed@localhost.localdomain>
2018-05-21 18:33:31 +0300

    php add

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
-
 	// Пользовательские функции
+    
+    $(".link-to-top").click(function() {
+        $('html, body').animate({
+                    scrollTop: $(".header").offset().top
+        }, 800);
+    });
 
+    $("#form").submit(function() {
+        $.ajax({
+            type: "POST",
+            url: "/mail.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+            $("#form").trigger("reset");
+        });
+            return false;
+    });
 });
---
+++ app/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+    $recepient = "ochtihy.ivan@yandex.ru";
+    $sitename = "Ветеринарная клиника";
+
+    $phone = trim($_POST["phone"]);
+    $text = trim($_POST["message"]);
+    $message = "Телефон: $phone \nТекст: $text";
+
+    $pagetitle = "Новая заявка с сайта \"$sitename\"";
+    mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
